16906f18fd4a2cc6a874a1861492e42697408a18
mykumishop <business@mykumi.com>
2025-07-31 21:32:21 +0200

    Update generate.mjs

--- generate.mjs
+++ generate.mjs
@@ -83,7 +83,7 @@ const run = async () => {
 
 // ✅ Tijdelijke trigger om Git altijd iets te laten committen
 await fs.writeFile(`dist/.force-${Date.now()}.txt`, 'force update');
-}
+};
 
 run().catch(err => {
   console.error('❌ Script mislukt:', err);
